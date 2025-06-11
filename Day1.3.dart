e81e21eddfbaea47ddb2597290ba32f9696963b7
aninda8680 <anindadebta8680@gmail.com>
2025-06-11 23:47:47 +0530

    Int

---
+++ Day1.3.dart
@@ -0,0 +1,22 @@
+void main() {
+  int age = 21;
+  print(age);
+
+  /*OUTPUT
+  21*/
+
+  int score = 100;
+  print(score);
+
+  score = 200;
+  print(score);
+
+  int bonus = 50;
+  int totalscore = score + bonus;
+  print(totalscore);
+
+  /*OUTPUT
+  100
+  200
+  250*/
+}
