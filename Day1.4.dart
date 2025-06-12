c0c760d5b28edc38a6dcfd82b67b5f7d4727a4fe
aninda8680 <anindadebta8680@gmail.com>
2025-06-12 21:45:40 +0530

    Double

---
+++ Day1.4.dart
@@ -0,0 +1,15 @@
+void main() {
+  double temperature = 98.6;
+  double weight = 7.5;
+  
+  print("Temperature: " + temperature.toString());
+  print("Weight: " + weight.toString());
+}
+
+
+
+
+
+/*OUTPUT
+  Temperature: 98.6
+  Weight: 7.5*/
