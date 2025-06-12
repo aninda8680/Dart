c067633c07d1b64c16344ea2f8620a5537d96050
aninda8680 <anindadebta8680@gmail.com>
2025-06-12 21:54:29 +0530

    Boolean ( true/false)

---
+++ Day2.1.dart
@@ -0,0 +1,15 @@
+void main() {
+  bool isMale = true;
+  bool isFemale = false;
+
+  print('Porfile Stats');
+  print('MALE:' + isMale.toString());
+  print('FEMALE:' + isFemale.toString());
+
+
+}
+
+/*OUTPUT
+Porfile Stats
+MALE:true
+FEMALE:false*/
