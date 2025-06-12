2429b26c6f7da44e6bb9da5947e3f97d3fc59943
aninda8680 <anindadebta8680@gmail.com>
2025-06-12 22:54:37 +0530

    Final (cannot be changed)

---
+++ Day2.3.dart
@@ -0,0 +1,15 @@
+void main() {
+  final int releaseYear = 2025;
+  final String creator = 'Google';
+  final bool isCompiledLanguage = true;
+
+  print('Dart was realeased in:' + releaseYear.toString());
+  print('Dart was created by :' + creator);
+  print('Is Dart a compiled Language? ' + isCompiledLanguage.toString());
+
+}
+
+/*OUTPUT
+Dart was realeased in:2025
+Dart was created by :Google
+Is Dart a compiled Language? true*/
