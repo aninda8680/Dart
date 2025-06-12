219dedaf9282eb37f8a3458883861a4518ce103b
aninda8680 <anindadebta8680@gmail.com>
2025-06-12 23:27:22 +0530

    Const

---
+++ Day2.4.dart
@@ -0,0 +1,20 @@
+void main() {
+  const String planetName = 'Mars';
+  const int numberOfMoons = 2;
+  const double distanceFromSum = 227.9;
+  const bool hasRings = false;
+
+  print('Planet Name: ' + planetName );
+  print('Number of Moons: + $numberOfMoons');
+  print('Distance from Sun: ' + distanceFromSum.toString() + ' million km');
+  print('Has Rings: ' + hasRings.toString());
+
+
+}
+
+
+/*OUTPUT
+Planet Name: Mars
+Number of Moons: + 2
+Distance from Sun: 227.9 million km
+Has Rings: false*/
