ce51dfb80831d41a4248893919b0b32cc077ae28
aninda8680 <anindadebta8680@gmail.com>
2025-06-12 22:24:59 +0530

    var

---
+++ Day2.2.dart
@@ -0,0 +1,13 @@
+void main() {
+  var name = 'Dart Programmer';
+  print(name);
+
+  var age = 21;
+  var price = 19.99;
+  var isActive = true;
+
+  print(age);
+  print(price);
+  print(isActive.toString());
+  print('Name: ' + '${name.toUpperCase()}');
+}
