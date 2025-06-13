64e49425ebfc936c9996745421e8e4a197ef3754
aninda8680 <anindadebta8680@gmail.com>
2025-06-14 01:10:00 +0530

    Naming Conventions

---
+++ Day3.1.dart
@@ -0,0 +1,20 @@
+void main() {
+  int x = 25;
+  double Y = 1.75;
+  const double ABC = 3.14;
+  final double temp123 = 100.0;
+  
+  // Print the variables with labels
+  print("Age: $x");
+  print("Height: $Y");
+  print("Pi Value: $ABC");
+  print("Max Temperature: $temp123");
+}
+
+/*OUTPUT
+
+Age: 25
+Height: 1.75
+Pi Value: 3.14
+Max Temperature: 100.0
+*/
