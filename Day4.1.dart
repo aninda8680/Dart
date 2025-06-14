9d8df8cb825017ec7d9b0a29a1d74448e85ac4aa
aninda8680 <anindadebta8680@gmail.com>
2025-06-14 22:05:57 +0530

    Operators ( Arithmetic Operators )

---
+++ Day4.1.dart
@@ -0,0 +1,27 @@
+void main() {
+  int num1 = 10;
+  int num2 = 5;
+  int sum = num1 + num2;
+  int difference = num1 - num2;
+  int product = num1 * num2;
+  double quotient = num1 / num2;
+
+  print('Sum: $sum');
+  print('Difference: $difference');
+  print('Product: $product');
+  print('Quotient: $quotient');
+
+}
+
+
+
+
+
+
+
+/*OUTPUT
+
+Sum: 15
+Difference: 5
+Product: 50
+Quotient: 2.0*/
